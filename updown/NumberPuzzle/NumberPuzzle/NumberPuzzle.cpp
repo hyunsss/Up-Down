5f688e106e03c906ba52943ba7d69b634d618f31
hyunsss <41880870+hyunsss@users.noreply.github.com>
2023-10-17 11:35:18 +0900

    Add files via upload

---
+++ updown/NumberPuzzle/NumberPuzzle/NumberPuzzle.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	int board[5][5];
+	int number = 1;
+	
+	//2차원 배열 초기화하기 (1, 2, 3, 4, 5 ---- )
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			board[i][j] = number;
+			number++;
+		}
+	}
+
+	//숫자 25가 들어있는 곳을 빈칸처럼 보이게 바꿈.
+	board[4][4] = 0;
+
+	//5x5사이즈의 패턴을 생성한다.
+	for (int i = 0; i < 5; i++) {
+ 
+	}
+	
+
+
+
+	
+	//칸마다 일정하지 않도록 랜덤으로 숫자를 배치한다.
+	
+
+}
+
---
+++ updown/UpDownGame/UpDownGame/UpDownGame.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    
+    //컴퓨터가 1~999사이의 정수를 랜덤으로 지정한다.
+    int Randomint = gen() % 1000;
+    //플레이어의 기회는 10번이다.
+    int PlayerChance = 10;
+    int PlayerChoice;
+    //지정된 숫자를 기준으로 플레이어가 수를 측정한다(작으면 Up 높으면 Down)
+        for (int i = PlayerChance; i > 0; PlayerChance--) {
+            if (PlayerChance == 0) {
+                cout << "기회를 다 소진했습니다. 정답은 : " << Randomint << endl;
+                break;
+        }
+            cout << "예상되는 숫자를 입력하십시오" << endl;
+            cin >> PlayerChoice;
+        if (PlayerChoice == Randomint) {
+            cout << "정답은 : "<< Randomint << endl;
+            cout << "축하합니다!! 정답을 맞추셨습니다!!!" << endl;
+            break;
+        }
+        else if (PlayerChoice > Randomint) {
+            cout << "숫자를 낮춰야 합니다!!" << endl;
+        }
+        else if (PlayerChoice < Randomint) {
+            cout << "숫자를 높여야 합니다!!" << endl;
+        }
+    }
+    //횟수를 다 하거나 맞출 경우 지정된 수를 보여주고 결과 메세지를 표시한다. 
+/*
+        //컴퓨터가 1~999사이의 정수를 랜덤으로 지정한다.
+        int Randomint = rand();
+        //플레이어의 기회는 10번이다.
+        int PlayerChance = 1;
+        int PlayerChoice;
+        //지정된 숫자를 기준으로 플레이어가 수를 측정한다(작으면 Up 높으면 Down)
+        for (int i = PlayerChance; i > 0; PlayerChance--) {
+            if (PlayerChance == 0) {
+                cout << "기회를 다 소진했습니다. 정답은 : " << Randomint << endl;
+                break;
+            }
+            cout << "예상되는 숫자를 입력하십시오" << endl;
+            cin >> PlayerChoice;
+            if (PlayerChoice == Randomint) {
+                cout << "정답은 : " << Randomint << endl;
+                cout << "축하합니다!! 정답을 맞추셨습니다!!!" << endl;
+                break;
+            }
+            else if (PlayerChoice > Randomint) {
+                cout << "숫자를 낮춰야 합니다!!" << endl;
+            }
+            else if (PlayerChoice < Randomint) {
+                cout << "숫자를 높여야 합니다!!" << endl;
+            }
+        }
+        //횟수를 다 하거나 맞출 경우 지정된 수를 보여주고 결과 메세지를 표시한다. 
+*/
+}
+
